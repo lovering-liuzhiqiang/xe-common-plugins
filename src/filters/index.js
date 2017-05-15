026633e06ae12b4d83b373f0e69e06bf889a81bb
wangsongtao <wangst@dingtalk.com>
2017-05-15 13:58:38 +0800

    增加常用功能处理方法

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,4 +1,4 @@
-exports.scaleLinks = function(url) {
+ export function scaleLinks(url) {
     if (!url) return;
     var urlTask = /^\//.test(url);
     var urlResult = url;
@@ -55,4 +55,54 @@ exports.scaleLinks = function(url) {
         default:
             return urlResult;
     };
+};
+ export function fnScaleLinks(url) {
+  if (!url) return;
+  var objs = ['/csc','/pdc','/rmc','/ofc','/dpc','/whc','/tfc','/epc','/ac'];
+  var regResult = false;
+  for (var i=0;i<objs.length;i++){
+    var patternUrl = new RegExp("^" + objs[i]);
+    regResult = patternUrl.test(url);
+    if(regResult){
+        break;
+    }
+  }
+  var urlResult = url;
+
+  switch (process.env.NODE_ENV) {
+    // 生产
+    case 'production':
+      if (regResult) {
+        return 'http://paas.xianyiscm.com/index#' + urlResult;
+      } else {
+        return 'http://paas-web.xianyiscm.com' + urlResult;
+      }
+      break;
+    // 预生产
+    case 'beta':
+      if (regResult) {
+        return 'http://paas-beta.xianyiscm.com/index#' + urlResult;
+      } else {
+        return 'http://paas-web-beta.xianyiscm.com' + urlResult;
+      }
+      break;
+    // 测试
+    case 'test':
+      if (regResult) {
+        return 'http://paas-test.xianyiscm.com/index#' + urlResult;
+      } else {
+        return 'http://paas-web-test.xianyiscm.com' + urlResult;
+      }
+      break;
+    // 开发版-devend
+    case 'devend':
+      if (regResult) {
+        return 'http://paas-dev.xianyiscm.com/index#' + urlResult;
+      } else {
+        return 'http://paas-web-dev.xianyiscm.com' + urlResult;
+      }
+      break;
+    default:
+      return urlResult;
+  };
 };
